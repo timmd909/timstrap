2e338704e7f1cb86436aeff91e09e833e2f75b0e
Tim Doerzbacher <tim@tim-doerzbacher.com>
2019-08-14 18:56:37 -0400

    Upgrade grunt sass options

--- grunt/config/sass.js
+++ grunt/config/sass.js
@@ -1,19 +1,35 @@
 'use strict';
 
 const sass = require('node-sass');
+const _ = require('lodash');
+
+const DEFAULT_OPTIONS = {
+  implementation: sass,
+  sourceMap: false,
+  includePaths: [
+    'node_modules/bootstrap/scss',
+    'src/scss',
+  ],
+};
+
+const MAIN_SOURCE = 'src/scss/timstrap.scss';
 
 module.exports = {
-  options: {
-    implementation: sass,
-    sourceMap: true,
-    includePaths: [
-      'node_modules/bootstrap/scss',
-      'src/scss',
-    ],
-  },
+  options: DEFAULT_OPTIONS,
   dist: {
+    options: _.defaults({
+      'outputStyle': 'compressed',
+    }, DEFAULT_OPTIONS),
     files: {
-      'dist/css/timstrap.css': 'src/scss/timstrap.css',
+      'dist/css/bootstrap.css': MAIN_SOURCE,
+    },
+  },
+  debug: {
+    options: _.defaults({
+      'outputStyle': 'nested',
+    }, DEFAULT_OPTIONS),
+    files: {
+      'dist/css/bootstrap.debug.css': MAIN_SOURCE,
     },
   },
 };
